9bf9a6447fdf1a698b419e510d6478fb6f7243f4
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-04-26 21:34:24 +0700

    Add more props

--- src/components/input_fields/BlockInputField.tsx
+++ src/components/input_fields/BlockInputField.tsx
@@ -1,14 +1,16 @@
 interface Props {
     type: string,
     placeholder: string,
-    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+    name?: string,
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-function BlockInputField({type, placeholder, onChange} : Props) {
+function BlockInputField({type, placeholder, name, onChange} : Props) {
     return (
         <input 
             type={type} 
             placeholder={placeholder} 
+            name={name}
             className={`outline-none w-full py-2 px-4 border border-gray-400 rounded-md text-sm`} 
             onChange={onChange}/>
     )
