0f409899bd9fc7789652410cc53f95ead3ce2cd3
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-04-26 21:33:54 +0700

    Add more props

--- src/components/buttons/BlockIconTextBtn.tsx
+++ src/components/buttons/BlockIconTextBtn.tsx
@@ -3,14 +3,18 @@ import React from "react"
 
 interface Props {
     icon: RemixiconComponentType,
-    text: string
+    text: string,
+    title: string,
+    onClick: () => void
 }
 
-function BlockIconTextBtn({ icon, text } : Props) {
+function BlockIconTextBtn({ icon, text, title, onClick } : Props) {
     const Icon = React.createElement(icon)
 
     return (
-        <button className={`w-fit bg-blue-700 text-white flex flex-row items-center py-2 px-2 rounded-md space-x-2 cursor-pointer`} title="Tambah Barang">
+        <button className={`w-fit bg-blue-700 text-white flex flex-row items-center py-2 px-2 rounded-md space-x-2 cursor-pointer`} 
+            title={title}
+            onClick={onClick}>
             <span className={`w-5`}>{Icon}</span>
             <span className={`text-sm font-semibold`}>{text}</span>
         </button>
